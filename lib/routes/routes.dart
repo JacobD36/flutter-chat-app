2377a515ff3d1f324fbbfe755d6d7a14edd479c5
Jaime Perez Frias <jperezfrias33@gmail.com>
2020-12-11 17:17:57 -0500

    Fin sección 7

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -6,7 +6,7 @@ import 'package:chat/pages/usuarios_page.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, Widget Function(BuildContext)> appRoutes = {
-  'usuarios': (_) => UsuariosPage(),
+'usuarios': (_) => UsuariosPage(),
   'chat': (_) => ChatPage(),
   'login': (_) => LoginPage(),
   'register': (_) => RegisterPage(),
